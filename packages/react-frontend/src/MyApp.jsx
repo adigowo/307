ca1c4b961a94c9002be340dc295efed1199e69bd
adigowo <ADIGO720@GMAIL.COM>
2024-04-10 12:45:12 -0700

    right before 9

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,45 +1,14 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
-const characters = [
-    {
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ];
+
 
   
   function MyApp() {
     const [characters, setCharacters] = useState([
-      {
-        name: "Charlie",
-        job: "Janitor"
-      }, // the rest of the data
-      {
-        name: "Mac",
-        job: "Bouncer"
-      },
-      {
-        name: "Dee",
-        job: "Aspring actress"
-      },
-      {
-        name: "Dennis",
-        job: "Bartender"
-      }
+      
     ]);
 
     function removeOneCharacter(index) {
@@ -50,13 +19,14 @@ const characters = [
       }
   
   
-  return (
-    <div className="container">
-      <Table
-        characterData={characters}
-        removeCharacter={removeOneCharacter}
-      />
-    </div>
+      return (
+        <div className="container">
+          <Table
+            characterData={characters}
+            removeCharacter={removeOneCharacter}
+          />
+          <Form />
+        </div>
   );
   }
 export default MyApp;
